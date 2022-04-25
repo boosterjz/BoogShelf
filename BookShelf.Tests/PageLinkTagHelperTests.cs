2b1859507c072e89f5d3b4e046c87709514679ad
Артём Завгородний <art.zavg@yandex.ru>
2022-04-25 13:47:19 +0300

    Добавлен тэг-хелпер со ссылками на страницы

---
+++ BookShelf.Tests/PageLinkTagHelperTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookShelf.Infrastructure;
+using BookShelf.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using Xunit;
+
+namespace BookShelf.Tests; 
+
+public class PageLinkTagHelperTests {
+    [Fact]
+    public void CanGeneratePageLinks() {
+        var urlHelper = new Mock<IUrlHelper>();
+        urlHelper.SetupSequence(x => x.Action(It.IsAny<UrlActionContext>()))
+            .Returns("Test/Page1")
+            .Returns("Test/Page2")
+            .Returns("Test/Page3");
+        var urlHelperFactory = new Mock<IUrlHelperFactory>();
+        urlHelperFactory.Setup(f => f.GetUrlHelper(It.IsAny<ActionContext>()))
+            .Returns(urlHelper.Object);
+        var helper = new PageLinkTagHelper(urlHelperFactory.Object) {
+            PageModel = new PagingInfo {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            },
+            PageAction = "Test"
+        };
+        var context = new TagHelperContext(
+            new TagHelperAttributeList(),
+            new Dictionary<object, object>(),
+            ""
+        );
+        var content = new Mock<TagHelperContent>();
+        var output = new TagHelperOutput("div", 
+            new TagHelperAttributeList(), 
+            (_, _) => Task.FromResult(content.Object));
+            
+        helper.Process(context, output);
+
+        Assert.Equal(@"<a href=""Test/Page1"">1</a>"
+                     + @"<a href=""Test/Page2"">2</a>"
+                     + @"<a href=""Test/Page3"">3</a>",
+            output.Content.GetContent());
+    }
+}
---
+++ BookShelf/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using BookShelf.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BookShelf.Infrastructure; 
+
+[HtmlTargetElement("div", Attributes = "page-model")]
+public class PageLinkTagHelper : TagHelper {
+    [ViewContext]
+    [HtmlAttributeNotBound]
+    public ViewContext ViewContext { get; set; }
+    public PagingInfo PageModel { get; set; }
+    public string PageAction { get; set; }
+    
+    private IUrlHelperFactory _urlHelperFactory;
+
+    public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory) {
+        _urlHelperFactory = urlHelperFactory;
+    }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output) {
+        var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+        var result = new TagBuilder("div");
+
+        for (var i = 1; i <= PageModel.TotalPages; i++) {
+            var tag = new TagBuilder("a") {
+                Attributes = {
+                    ["href"] = urlHelper.Action(PageAction, new {page = i})
+                }
+            };
+            tag.InnerHtml.Append(i.ToString());
+            result.InnerHtml.AppendHtml(tag);
+        }
+
+        output.Content.AppendHtml(result.InnerHtml);
+    }
+}
